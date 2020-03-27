34990e6c3c0ea829d96a609fe674042c1ba7cecd
Saddam Husen <saddam.husen1997@gmail.com>
2020-03-28 00:32:19 +0530

    NA

--- src/_config/index.js
+++ src/_config/index.js
@@ -1,6 +1,5 @@
 export const CONST = {
-  //BACKEND_URL: 'http://localhost:5000/api/dadmin',
-  BACKEND_URL: 'http://139.162.210.64/api/dadmin',
+  BACKEND_URL: 'http://localhost:5000/api/v1',
   APP_NAME: 'React Admin',
   FOOTER_TEXT: '© 2019-2020 React Admin. All rights reserved.',
 };
